b9c2d515e4a347a1c43615dabc3256490d10348d
Junki <wnsrl1228@naver.com>
2022-03-20 16:31:00 +0900

    28

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -16,10 +16,14 @@ class User {
         }
         return {success:false,msg:"존재안함"};
     }
-    register() {
+    async register() {
         const client = this.body;
-        const response = UserStorage.save(client);
-        return response
+        try{
+            const response = await UserStorage.save(client);
+            return response;
+        }catch(err){
+            return {success:false, msg : err};
+        }
     }
 
 }
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -15,10 +15,10 @@ class UserStorage {
         return userInfo;
     }
 
-    static getUsers(...fields) {
-        // const users = this.#users;
-
+    static #getUsers(data,isAll, fields){
         // reduce( callback(처음값 그리고 누적값, 두번째부터 쭉,인덱스 ),초기값)
+        const users = JSON.parse(data);
+        if(isAll) return users;
         const newUsers = fields.reduce((newUsers, field)=>{
             if (users.hasOwnProperty(field)){
                 newUsers[field] = users[field];
@@ -26,7 +26,14 @@ class UserStorage {
             // return 값은 다음 newUsers값에 들어감
             return newUsers;
         },{});
-        return newUsers
+        return newUsers       
+    }
+    static getUsers(isAll, ...fields) {
+        return fs.readFile("./src/databases/users.json")
+        .then((data) => {
+            return this.#getUsers(data,isAll,fields);
+        })
+        .catch(console.error);
     }
 
     static getUserInfo(id){
@@ -38,12 +45,16 @@ class UserStorage {
     }
 
 
-    static save(userInfo) {
-        // const users = this.#users;
-        // users.id.push(userInfo.id);
-        // users.name.push(userInfo.name);
-        // users.pwd.push(userInfo.pwd);
-        // return {success:true};
+    static async save(userInfo) {
+        const users = await this.getUsers(true);
+        if(users.id.includes(userInfo.id)) {
+            throw "이미 존재하는 아이디";
+        }
+        users.id.push(userInfo.id);
+        users.name.push(userInfo.name);
+        users.pwd.push(userInfo.pwd);
+        fs.writeFile("./src/databases/users.json",JSON.stringify(users));
+        return {success :true};
     }
 }
 module.exports = UserStorage;
